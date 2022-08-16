f68c1e490ff2ce292ec47592c487e02da179d4e0
ADITYA PATEL <adipatel016@gmail.com>
2022-08-17 00:32:24 +0530

    I have completed PRACTICAL1_2.kt

---
+++ src/main/kotlin/PRACTICAL1_2.kt
@@ -0,0 +1,20 @@
+fun main(){
+    val a:Int = 30;
+    val b:Float = 2.5F
+    val c:String = "ADITYA"
+    val d:Boolean = true
+    val e:Double = 65.65
+    val f:Char = 'A'
+    val g:Long = 20012011076
+    val h:Short = -69
+    val i:Byte = 125
+    println("Integer Value : $a")
+    println("Float Value : $b")
+    println("string Value : $c")
+    println("Boolean Value : $d")
+    println("Double Value : $e")
+    println("Character Value : $f")
+    println("Long Value : $g")
+    println("Short Value : $h")
+    println("Byte Value : $i")
+}
