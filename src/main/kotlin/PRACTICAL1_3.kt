a0aeeb651bdef14e42302b61eea1446283f38cdb
ADITYA PATEL <adipatel016@gmail.com>
2022-08-23 22:13:33 +0530

    I have completed PRACTICAL1_3.kt

---
+++ src/main/kotlin/PRACTICAL1_3.kt
@@ -0,0 +1,11 @@
+fun main(){
+    var a:Int = 76
+    var b:Double = a.toDouble()
+    var c:String = "450"
+    var d:Int = c.toInt()
+    var e:Double = c.toDouble()
+    println("20012011076 \n")
+    println("Integer to Double : $a to $b")
+    println("String to Integer : $c to $d")
+    println("String to Double : $c to $e")
+}
