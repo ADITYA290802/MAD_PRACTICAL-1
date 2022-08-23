025796ec72d7b15f806a653aca38e1228dd092e6
ADITYA PATEL <adipatel016@gmail.com>
2022-08-23 22:57:23 +0530

    I have completed PRACTICAL1_4.kt

---
+++ src/main/kotlin/PRACTICAL1_4.kt
@@ -0,0 +1,12 @@
+fun main() {     var i = 15
+    var j = 30
+    println("20012011076 \n")
+    println("Before Swapping:")
+    println("i=$i")
+    println("j=$j")
+    i = i + j
+    j= i - j
+    i = i - j
+    println("After Swapping:")
+    println("i=$i")
+    println("j=$j") }
